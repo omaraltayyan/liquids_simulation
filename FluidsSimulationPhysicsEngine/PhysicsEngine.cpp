a133f8f4c3e41ea0832c9fed3191873f5236cb52
omar altayyan <omaraltayyan@gmail.com>
2019-05-06 12:09:48 +0300

    implemented a very basic thread pooling mechanism for processing bodies updates over multiple cores concurrently

--- FluidsSimulationPhysicsEngine/PhysicsEngine.cpp
+++ FluidsSimulationPhysicsEngine/PhysicsEngine.cpp
@@ -1,6 +1,97 @@
 #include "PhysicsEngine.h"
-#include "qvector2d.h"
+#include <windows.h>
+
+void* engineUpdateLoopThreadFunction(void * engineArg, int threadIndex)
+{
+	((PhysicsEngine*)engineArg)->engineUpdateLoop(threadIndex);
+	return 0;
+}
+
+PhysicsEngine::~PhysicsEngine() {
+	this->shouldStopEngine = true;
+	for (int i = 0; i < this->runningThreads; i++)
+	{
+		if (this->engineUpdateLoopThreads[i]->joinable())
+		{
+			this->engineUpdateLoopThreads[i]->join();
+		}
+		delete this->engineUpdateLoopThreads[i];
+	}
+
+	delete this->engineUpdateLoopThreads;
+
+}
 
 PhysicsEngine::PhysicsEngine()
 {
+	this->shouldStopEngine = false;
+	this->shouldRunLoop = false;
+	this->runningThreads = max(1, thread::hardware_concurrency());
+
+	this->engineUpdateLoopThreads = new thread*[runningThreads];
+	for (int i = 0; i < runningThreads; i++)
+	{
+		engineUpdateLoopThreads[i] = new thread(engineUpdateLoopThreadFunction, this, i);
+	}
+}
+
+void PhysicsEngine::resumeEngine() {
+	this->shouldRunLoop = true;
+	std::lock_guard<std::mutex> lock(shouldBeProcessingNextUpdateLoopLocker);
+	shouldBeProcessingNextUpdateLoopConditional.notify_all();
+}
+
+void PhysicsEngine::pauseEngine() {
+	this->shouldRunLoop = false;
+	std::lock_guard<std::mutex> lock(shouldBeProcessingNextUpdateLoopLocker);
+	shouldBeProcessingNextUpdateLoopConditional.notify_all();
+}
+
+void PhysicsEngine::engineUpdateLoop(int threadIndex) {
+	while (!shouldStopEngine)
+	{
+		{
+			// wait until we are allowed to process the next update or the engine stops
+			std::unique_lock<std::mutex> lock(shouldBeProcessingNextUpdateLoopLocker);
+			shouldBeProcessingNextUpdateLoopConditional.wait(lock, [&] {return this->shouldRunLoop || shouldStopEngine; });
+		}
+
+		// stop the loop when exiting the thread is required
+		if (shouldStopEngine) {
+			break;
+		}
+
+		threadsProcessingCurrentLoop++;
+
+		this->runUpdateBatch(threadIndex);
+
+
+		threadsProcessingCurrentLoop--;
+
+		{
+			// notify all other threads that the current one is finished
+			std::lock_guard<std::mutex> lock(shouldBeProcessingNextUpdateLoopLocker);
+			shouldBeProcessingNextUpdateLoopConditional.notify_all();
+		}
+
+		{
+			// wait until all other threads finish processing
+			std::unique_lock<std::mutex> lock(shouldBeProcessingNextUpdateLoopLocker);
+			shouldBeProcessingNextUpdateLoopConditional.wait(lock, [&] {return threadsProcessingCurrentLoop == 0; });
+		}
+
+
+		if (threadIndex == 0) {
+			this->applyUpdates();
+		}
+		
+	}
+}
+
+void PhysicsEngine::runUpdateBatch(int threadIndex) {
+
+}
+
+void PhysicsEngine::applyUpdates() {
+
 }
--- FluidsSimulationPhysicsEngine/PhysicsEngine.h
+++ FluidsSimulationPhysicsEngine/PhysicsEngine.h
@@ -1,9 +1,43 @@
 #pragma once
 
 #include "fluidssimulationphysicsengine_global.h"
+#include "qrect.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+using namespace std;
 
 class FLUIDSSIMULATIONPHYSICSENGINE_EXPORT PhysicsEngine
 {
+	thread** engineUpdateLoopThreads;
+
+	mutex bodiesAccessLock;
+
+	friend void* engineUpdateLoopThreadFunction(void * engineArg, int threadIndex);
+
+	atomic<bool> shouldRunLoop;
+	atomic<bool> shouldStopEngine;
+
+	condition_variable shouldBeProcessingNextUpdateLoopConditional;
+	mutex shouldBeProcessingNextUpdateLoopLocker;
+
+	void engineUpdateLoop(int threadIndex);
+
+	int runningThreads;
+
+	atomic<int> threadsProcessingCurrentLoop;
+
+	void runUpdateBatch(int threadIndex);
+	
+	void applyUpdates();
+
 public:
+
+	qreal timeDelta = 1.0 / 64.0;
+
+	void resumeEngine();
+	void pauseEngine();
+
 	PhysicsEngine();
+	~PhysicsEngine();
 };
