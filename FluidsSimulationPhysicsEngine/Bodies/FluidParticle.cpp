b42168262c438bfab359e44fd33c4e5bf1c9ffd7
Muha94 <muha94ma@gmail.com>
2019-05-06 10:54:00 +0300

    started working on the propeties of fluid particle class

--- FluidsSimulationPhysicsEngine/Bodies/FluidParticle.cpp
+++ FluidsSimulationPhysicsEngine/Bodies/FluidParticle.cpp
@@ -2,8 +2,13 @@
 
 
 
-FluidParticle::FluidParticle()
+FluidParticle::FluidParticle(double dynsity, double viscosity, double pressure,double mass, float Xposition, float Yposition)
 {
+	_dynsity = dynsity;
+	_viscosity = viscosity;
+	_pressure = pressure;
+	_mass = mass;
+	_position = new QVector2D(Xposition, Yposition);
 }
 
 
--- FluidsSimulationPhysicsEngine/Bodies/FluidParticle.h
+++ FluidsSimulationPhysicsEngine/Bodies/FluidParticle.h
@@ -1,11 +1,22 @@
 #pragma once
 
 #include "Particle.h"
+#include <QVector2D>
 
 class FluidParticle: public Particle
 {
 public:
-	FluidParticle();
+	FluidParticle(double dynsity,double viscosity,double pressure,double mass,float Xposition,float Yposition);
 	~FluidParticle();
+private:
+	double _dynsity;
+	double _viscosity;
+	double _pressure;
+	double _mass;
+	QVector2D* _velocity;
+	QVector2D* _position;
+	QVector2D* _accelration;
+	
+	 
 };
 
