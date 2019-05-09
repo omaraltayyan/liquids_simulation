7a7e517ccea69d0c421dd5004460b17b8fc154c2
Muha94 <muha94ma@gmail.com>
2019-05-09 23:38:21 +0300

    Added the smooting kernel equations in order to start working on the fluid propeties's values

--- FluidsSimulationPhysicsEngine/Bodies/FluidParticle.cpp
+++ FluidsSimulationPhysicsEngine/Bodies/FluidParticle.cpp
@@ -1,17 +1,46 @@
 #include "FluidParticle.h"
+#include <qmath.h>
 
 
 
-FluidParticle::FluidParticle(double dynsity, double viscosity, double pressure,double mass, float Xposition, float Yposition)
-{
-	_dynsity = dynsity;
+FluidParticle::FluidParticle( double viscosity, double mass, float Xposition, float Yposition)
+{	
 	_viscosity = viscosity;
-	_pressure = pressure;
 	_mass = mass;
 	_position = new QVector2D(Xposition, Yposition);
 }
 
 
 FluidParticle::~FluidParticle()
+{
+}
+
+
+double FluidParticle::applyKernal(double distance, double radius, SmoothingKernals kernal)
+{
+	
+		switch (kernal)
+		{
+		case poly6:
+			return (315 / (64*M_PI*qPow(radius,9))) * qPow(((radius*radius) - (distance*distance)),3);			
+		case spiky:
+			return (15 / (M_PI*qPow(radius, 6))) * qPow((radius - distance), 3);
+		case visc:
+			return (15 / (2 * M_PI*qPow(radius, 3))) *
+				(-(qPow(distance, 3) / (2 * M_PI*qPow(radius, 3))) + 
+				(qPow(distance, 2) / qPow(radius, 2)) + (radius / (2 * distance)) - 1);
+		default:
+			return 0.0;
+		}		
+	
+
+}
+
+double FluidParticle::computeDynsity(QVector<BodiesVector*>& neighboringBodies,double radius)
+{
+	return 0.0;
+}
+
+void FluidParticle::updateDynsity()
 {
 }
--- FluidsSimulationPhysicsEngine/Bodies/FluidParticle.h
+++ FluidsSimulationPhysicsEngine/Bodies/FluidParticle.h
@@ -2,12 +2,22 @@
 
 #include "Particle.h"
 #include <QVector2D>
+#include <qmath.h>
+#include "Grid.h"
 
+
+enum SmoothingKernals
+{
+	poly6,
+	spiky,
+	visc
+};
 class FluidParticle: public Particle
 {
 public:
-	FluidParticle(double dynsity,double viscosity,double pressure,double mass,float Xposition,float Yposition);
+	FluidParticle(double viscosity,double mass,float Xposition,float Yposition);
 	~FluidParticle();
+	void updateDynsity();
 private:
 	double _dynsity;
 	double _viscosity;
@@ -16,6 +26,8 @@ private:
 	QVector2D* _velocity;
 	QVector2D* _position;
 	QVector2D* _accelration;
+	double applyKernal(double distance, double radius, SmoothingKernals kernal);
+	double computeDynsity(QVector<BodiesVector*>& neighboringBodies,double radius);
 	
 	 
 };
