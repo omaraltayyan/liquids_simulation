2d9f862d7d320f16a2133df36f26d6418eed88d7
Muha94 <muha94ma@gmail.com>
2019-05-12 22:34:20 +0300

    made a few changes to fluid particle class properties

--- FluidsSimulationPhysicsEngine/Bodies/FluidParticle.h
+++ FluidsSimulationPhysicsEngine/Bodies/FluidParticle.h
@@ -19,13 +19,8 @@ public:
 	FluidParticle(double viscosity, double mass, float Xposition, float Yposition);
 	~FluidParticle();
 private:
-	double _dynsity;
-	double _viscosity;
-	double _pressure;
-	double _mass;
-	QVector2D _velocity;
-	QVector2D _position;
-	QVector2D _accelration;
+	double _dynsity, _viscosity, _pressure, _mass;
+	QVector2D _velocity, _position, _force, _tempVelocity, _tempForce;
 	double applyKernal(double distance, double radius, SmoothingKernals kernal);
 	double computeDynsity(QVector<BodiesVector*> surroundingBodies, double radius);
 	double computePressure(double gasConstant, double restDynsity, double dynsity);
